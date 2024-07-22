158f55eb819ea77737e8199d610759d7ee54a01e
SparrowMike <michal.m_wroblewski@hotmail.com>
2024-07-22 16:44:54 +0800

    test: 💍 disable side columns

--- keyboards/bastardkb/tbkmini/keymaps/mine/keymap.c
+++ keyboards/bastardkb/tbkmini/keymaps/mine/keymap.c
@@ -32,26 +32,26 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_ESC,
+        KC_TRNS, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,   KC_TRNS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        LCTL_T(KC_ESC), HM_A,HM_S ,HM_D,    HM_F,    KC_G,                           KC_H,    HM_J,    HM_K,    HM_L, HM_SCLN, KC_QUOT,
+        KC_TRNS, HM_A,    HM_S,    HM_D,    HM_F,    KC_G,                           KC_H,    HM_J,    HM_K,    HM_L,   HM_SCLN,  KC_TRNS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M, KC_COMM, KC_DOT, KC_SLSH,TD(TD_RALT_CAPS),
+        KC_TRNS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                           KC_N,    KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,  KC_TRNS,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_ESC,   MO(_FIRST),  KC_ENT,   KC_SPC,   KC_BSPC, MO(_SECOND)
+                                            KC_ESC,  MO(_FIRST),  KC_ENT,   KC_SPC,  KC_BSPC, MO(_SECOND)
                                             //`--------------------------'  `--------------------------'
 
         ),
 
     [_FIRST] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                        KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_LALT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_MINS, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_PIPE,
+        KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                           KC_MINS, KC_EQL,  KC_GRV,  KC_LBRC, KC_RBRC, KC_TRNS,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         KC_TRNS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                           KC_UNDS, KC_QUOT, KC_TILD, KC_PIPE, KC_BSLS, KC_TRNS,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_TRNS,    KC_TRNS,  KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS
+                                            KC_TRNS, KC_TRNS,  KC_TRNS,      BK_TAB, KC_TAB, KC_ESC
                                             //`--------------------------'  `--------------------------'
 
     ),
